a4254d784e8e6e36b40307e8c197ed8d3e307cf2
parandyk <mparandyk@interia.pl>
2025-06-06 18:54:18 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 mode=$1
-model=$2
-chkpt=$3
-GPUS=$4
-dir=$5
+dataset=$2
+model=$3
+chkpt=$4
+GPUS=$5
+dir=$6
 
 cd $dir
 
@@ -10,13 +11,13 @@ cd $dir
 
 if [ "$mode" == train ]
 then
-  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --summary-dir --use-amp --seed=0
+  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_${dataset}.yml --summary-dir --use-amp --seed=0
 elif [ "$mode" == tune ]
 then
-  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --summary-dir --use-amp --seed=0 -t $chkpt
+  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_${dataset}.yml --summary-dir --use-amp --seed=0 -t $chkpt
 elif [ "$mode" == test ]
 then
-  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --summary-dir --test-only -r $chkpt
+  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_${dataset}.yml --summary-dir --test-only -r $chkpt
 fi
 
 
