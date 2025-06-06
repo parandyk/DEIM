753e9509e8bc27225184087e3fa6aa099b2cf603
parandyk <mparandyk@interia.pl>
2025-06-06 18:17:41 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -13,10 +13,10 @@ then
   torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --use-amp --seed=0
 elif [ "$mode" == tune ]
 then
-  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --use-amp --seed=0 --test-only -t $chkpt
+  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --use-amp --seed=0 -t $chkpt
 elif [ "$mode" == test ]
 then
-  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --use-amp --seed=0 -r $chkpt
+  torchrun --master_port=7777 --nproc_per_node=$GPUS train.py -c configs/deim_dfine/deim_hgnetv2_${model}_coco.yml --use-amp --seed=0 --test-only -r $chkpt
 fi
 
 
